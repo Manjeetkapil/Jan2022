24c37aae99534760d9bd9d4fde491672fe04d390
Manjeetkapil <kumar2001kapilm@gmail.com>
2022-01-30 15:34:33 +0530

    setup first enact app

---
+++ Jan24-28/enact/first/src/App/App.js
@@ -0,0 +1,30 @@
+import kind from '@enact/core/kind';
+import ThemeDecorator from '@enact/sandstone/ThemeDecorator';
+import Panels from '@enact/sandstone/Panels';
+
+import MainPanel from '../views/MainPanel';
+
+import css from './App.module.less';
+
+const App = kind({
+	name: 'App',
+
+	styles: {
+		css,
+		className: 'app'
+	},
+
+	render: (props) => (
+		<div {...props}>
+			<Panels>
+				<MainPanel />
+			</Panels>
+			<div className={css.area}>
+				New area
+			</div>
+		</div>
+	)
+});
+
+export default ThemeDecorator(App);
+// export default App;
---
+++ Jan24-28/enact/first/src/views/MainPanel.js
@@ -0,0 +1,17 @@
+import Button from '@enact/sandstone/Button';
+import kind from '@enact/core/kind';
+import {Panel, Header} from '@enact/sandstone/Panels';
+
+const MainPanel = kind({
+	name: 'MainPanel',
+
+	render: (props) => (
+		<Panel {...props}>
+			{/* <Header title="Hello world!" /> */}
+			<Header title="Zinda h toh"/>
+			{/* <Button>Click me</Button> */}
+		</Panel>
+	)
+});
+
+export default MainPanel;
