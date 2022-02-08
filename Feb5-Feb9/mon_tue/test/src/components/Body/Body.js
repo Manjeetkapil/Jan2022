b7870b8c2ac4f2fb197c757b43e4f80be535ef19
Manjeetkapil <kumar2001kapilm@gmail.com>
2022-02-08 19:36:38 +0530

    Added funtionality of delete photos

--- Feb5-Feb9/mon_tue/test/src/components/Body/Body.js
+++ Feb5-Feb9/mon_tue/test/src/components/Body/Body.js
@@ -1,109 +1,58 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 import "./Body.css"
-import First from "../../images/first.jpeg";
+import Delete from "../../images/delete.png";
 
-function Body() {
-  return (
+class Body extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      data:[],
+    }
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=100')
+    .then((response)=>response.json())
+    .then(response=>{
+      this.setState({data:response})
+      console.log(response);
+    })
+  }
+  handleClick = (id) =>{
+    function removeClicked(item){
+      if (item.id === id){
+        return false;
+      }
+      return true;
+    }
+    this.setState({data:this.state.data.filter(removeClicked)})
+  }
+
+  render() {
+    return (
+    <Container>
     <div className="mybody">
-      <Card className="card" style={{ width: "50rem" }}>
-        <Card.Body>
-          <Card.Title>Lg Soft</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the Lg Soft and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <img src={First} alt="" style={{width:"100px",height:"100px"}}/>
-          <br />
-          <Card.Link href="#">New</Card.Link>
-          <Card.Link href="#">GO</Card.Link>
-        </Card.Body>
-      </Card>
-      <Card className="card" style={{ width: "50rem" }}>
-        <Card.Body>
-          <Card.Title>Lg Soft</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the Lg Soft and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <img src={First} alt="" style={{width:"100px",height:"100px"}}/>
-          <br />
-          <Card.Link href="#">New</Card.Link>
-          <Card.Link href="#">GO</Card.Link>
-        </Card.Body>
-      </Card>
-      <Card className="card" style={{ width: "50rem" }}>
-        <Card.Body>
-          <Card.Title>Lg Soft</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the Lg Soft and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <img src={First} alt="" style={{width:"100px",height:"100px"}}/>
-          <br />
-          <Card.Link href="#">New</Card.Link>
-          <Card.Link href="#">GO</Card.Link>
-        </Card.Body>
-      </Card>
-      <Card className="card" style={{ width: "50rem" }}>
-        <Card.Body>
-          <Card.Title>Lg Soft</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the Lg Soft and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <img src={First} alt="" style={{width:"100px",height:"100px"}}/>
-          <br />
-          <Card.Link href="#">New</Card.Link>
-          <Card.Link href="#">GO</Card.Link>
-        </Card.Body>
-      </Card>
-      <Card className="card" style={{ width: "50rem" }}>
-        <Card.Body>
-          <Card.Title>Lg Soft</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the Lg Soft and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <img src={First} alt="" style={{width:"100px",height:"100px"}}/>
-          <br />
-          <Card.Link href="#">New</Card.Link>
-          <Card.Link href="#">GO</Card.Link>
-        </Card.Body>
-      </Card>
-      <Card className="card" style={{ width: "50rem" }}>
-        <Card.Body>
-          <Card.Title>Lg Soft</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the Lg Soft and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <img src={First} alt="" style={{width:"100px",height:"100px"}}/>
-          <br />
-          <Card.Link href="#">New</Card.Link>
-          <Card.Link href="#">GO</Card.Link>
-        </Card.Body>
-      </Card>
+      {this.state.data.map((item)=>(
+        <Card className="card" key={item.id}>
+          <Card.Body>
+            <Card.Title>
+              {item.title}
+              <img onClick={()=>this.handleClick(item.id)} className="delete" src={Delete} alt="" style={{width:"100px",height:"100px"}}/>
+            </Card.Title>
+            <img src={item.url} alt="" style={{width:"100px",height:"100px",marginLeft:"100px"}}/>
+            <br />
+          </Card.Body>
+        </Card>
+      ))}
     </div>
-  );
+    </Container>
+    );
+  }
 }
 
 export default Body;
